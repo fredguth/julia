a8b136d46c763683832de7a945065a9fe0b85610
Fred Guth <fredguth@fredguth.com>
2016-07-21 00:33:05 -0300

    improv select

--- app/routes/pacientes/new.js
+++ app/routes/pacientes/new.js
@@ -3,19 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
-    debugger;
     this.set('paciente', this.store.createRecord('paciente'));
     return this.store.findAll('consultorio').then((consultorios)=>{
       let entry = this.get('paciente.descriptions').findBy('key', 'consultorio_preferencia');
       entry['options'] = consultorios.toArray();
-      Ember.Logger.debug('descriptions', entry);
       this.set('paciente.selected', null);
       return this.get('paciente');
     });
-
-
-
-
 
   },
   actions: {
@@ -23,7 +17,7 @@ export default Ember.Route.extend({
       this.store.findRecord('consultorio', paciente.get('selected.id')).then((consultorio)=> {
         paciente.set('consultorio_preferencia', consultorio);
         paciente.set('criado_em' , new Date());
-        paciente.save();
+        consultorio.get('pacientes').pushObject(paciente.save());
       })
 
       this.transitionTo('pacientes.show', paciente);
