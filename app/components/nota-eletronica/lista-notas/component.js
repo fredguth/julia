fde6772c79c49c5fbd0594983ed0f82c61ffb788
Denis Toledo <contato@denistoledo.com.br>
2017-03-31 16:25:56 -0300

    emitir comprovante

--- app/components/nota-eletronica/lista-notas/component.js
+++ app/components/nota-eletronica/lista-notas/component.js
@@ -3,5 +3,7 @@ const { Component } = Ember;
 
 export default Ember.Component.extend({
 	tagName: 'li',
-	classNames: ['lista-notas']
+	classNames: ['lista-notas'],
+	status: 'emitida', //emitida, aguardando, processando
+	type: 'nota', // nota, registro
 });
